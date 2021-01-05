99a393e635bfcd4e15d7e84100a669917703cf0b
thom <you@example.com>
2021-01-06 00:11:45 +0100

    javascript 5 af

---
+++ javascript5/quiz2.js
@@ -0,0 +1,77 @@
+const quiz2 = 
+{
+    "quizID": "Math 1",
+    "quizMetaData":
+    {
+        "title": "thom zijn quiz",
+        "imageURI": "https://cdn.discordapp.com/attachments/717834533738643492/788713290662543360/unknown.png?crop=0.987xw:0.790xh;0.00160xw,0.128xh&resize=980:*"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "hoelang was cyberpunk 2077 in development?  ",
+				"answers": [{
+						"answer": "6 jaar",
+						"feedback": false
+					},
+					{
+						"answer": "7 jaar",
+						"feedback": true
+					},
+					{
+						"answer": "8 jaar",
+						"feedback": false
+					},
+					{
+						"answer": "9 jaar",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "hoe oud is skyrim",
+				"answers": [{
+						"answer": "8 jaar",
+						"feedback": false
+					},
+					{
+						"answer": "9 jaar",
+						"feedback": true
+					},
+					{
+						"answer": "10 jaar",
+						"feedback": false
+					},
+					{
+						"answer": "11 jaar",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "wie won de gp van Abu Dhabi van 2020 f1",
+				"answers": [{
+						"answer": "Hamilton",
+						"feedback": false
+					},
+					{
+						"answer": "Bottas",
+						"feedback": false
+					},
+					{
+						"answer": "Verstappen",
+						"feedback": true
+					},
+					{
+						"answer": "vettel",
+						"feedback": false
+					},
+					{
+						"answer": "Russel",
+						"feedback": false
+					},
+				]
+			}
+		]
+	}
